fedb6cc7eaa16dbc6ef9eba211e3c4aacfb2542c
ken5scal <kengoscal@gmail.com>
2016-08-31 00:02:12 +0900

    Add http endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"runtime"
+	"net/http"
+	"github.com/ken5scal/meander"
+	"encoding/json"
+)
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	// meander.APIKey = "TODO"
+	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+		respond(w,r,meander.Journeys)
+	})
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	return json.NewEncoder(w).Encode(data)
 }
