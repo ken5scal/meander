884608ced3e00561b8205f1d3ddd84e1b8bd9da6
ken5scal <kengoscal@gmail.com>
2016-08-31 00:02:47 +0900

    Remove old data

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"runtime"
 	"net/http"
 	"github.com/ken5scal/meander"
 	"encoding/json"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
 	// meander.APIKey = "TODO"
 	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
-		respond(w,r,meander.Journeys)
+		respond(w, r, meander.Journeys)
 	})
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
