7b685b1648f93f2a477d062bbf2d864f870bb3bc
ken5scal <kengoscal@gmail.com>
2016-09-02 09:06:54 +0900

    Add assertion test for enum

---
+++ cost_level_test.go
@@ -0,0 +1,16 @@
+package meander_test
+
+import "testing"
+import (
+	"github.com/ken5scal/meander"
+	"github.com/cheekybits/is"
+)
+
+func TestCostValues(t *testing.T) {
+	is := is.New(t)
+	is.Equal(int(meander.Cost1), 1)
+	is.Equal(int(meander.Cost2), 2)
+	is.Equal(int(meander.Cost3), 3)
+	is.Equal(int(meander.Cost4), 4)
+	is.Equal(int(meander.Cost5), 5)
+}
